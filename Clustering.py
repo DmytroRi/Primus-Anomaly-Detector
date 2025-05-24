0688a6d748cb8a8715d92ea45f1b57b3363812ae
Dmytro Riabchuk <dmytro.r.v@gmail.com>
2025-05-24 12:51:00 +0200

    New file for algorithms logic

---
+++ Clustering.py
@@ -0,0 +1,10 @@
+
+def split_data():
+    """
+    Splits the dataset into training, validation, and test sets.
+    """
+    print("Splitting data into training, validation, and test sets...")
+    
+    data = upload_data_from_db();
+    
+    pass
